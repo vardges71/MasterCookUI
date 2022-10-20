88d85347a2bbfc4e5c7b05b9997b2331ff88ccc4
vardges71 <vardges71@gmail.com>
2022-10-20 13:36:28 -0400

    Initial Commit

---
+++ MyMasterCook/MyMasterCookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMasterCookApp.swift
+//  MyMasterCook
+//
+//  Created by Vardges Gasparyan on 2022-10-20.
+//
+
+import SwiftUI
+
+@main
+struct MyMasterCookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
