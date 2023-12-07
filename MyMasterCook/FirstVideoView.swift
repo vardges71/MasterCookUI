5ad5d30559692097bc445781f69ddc043b0e6f73
vardges71 <vardges71@gmail.com>
2023-12-07 14:53:11 -0500

    Updated 07.12.2023

---
+++ MyMasterCook/FirstVideoView.swift
@@ -0,0 +1,20 @@
+//
+//  FirstVideoView.swift
+//  MyMasterCook
+//
+//  Created by Vardges Gasparyan on 2023-09-03.
+//
+
+import SwiftUI
+
+struct FirstVideoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FirstVideoView_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstVideoView()
+    }
+}
