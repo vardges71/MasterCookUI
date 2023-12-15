5906ecf1538478814a588300ad240fe92c121668
vardges71 <vardges71@gmail.com>
2023-12-15 01:27:04 -0500

    auth finished

---
+++ MasterCook/Login/ForgotButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ForgotButtonView.swift
+//  MasterCook
+//
+//  Created by Vardges Gasparyan on 2023-12-14.
+//
+
+import SwiftUI
+
+struct ForgotButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForgotButtonView()
+}
---
+++ MasterCook/Login/RePasswordTextFieldView.swift
@@ -0,0 +1,18 @@
+//
+//  RePasswordTextFieldView.swift
+//  MasterCook
+//
+//  Created by Vardges Gasparyan on 2023-12-14.
+//
+
+import SwiftUI
+
+struct RePasswordTextFieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RePasswordTextFieldView()
+}
